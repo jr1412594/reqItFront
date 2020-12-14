b2a72245d3d99bcf339b659ecefc9c5829e56e0a
jr1412594 <jr.medina1412@gmail.com>
2020-12-14 13:57:39 -0700

    mock fetch

--- App.js
+++ App.js
@@ -2,7 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const baseUrl = 'http://localhost:7000/items/'
+
 export default function App() {
+
+  
+
+  fetch(baseUrl)
+    .then(response => response.json())
+    .then(console.log())
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
