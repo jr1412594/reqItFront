ed7117c41ca112323d12402d2687be0087681c99
jr1412594 <jr.medina1412@gmail.com>
2021-01-02 23:30:38 -0700

    camera console log

--- components/CameraComponent.js
+++ components/CameraComponent.js
@@ -1,10 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
+import { Feather as Icon } from "@expo/vector-icons"
 
 export default function CameraComponent() {
     const [hasPermission, setHasPermission] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
+    const cameraRef = useRef();
+
+    const takePicture = async() => {
+        if (cameraRef.current) {
+            const options = {quality: 0.5, base64: true, };
+
+            const picture = await cameraRef.current.takePictureAsync(options);
+
+            // console.log(cameraRef.current.getSupportdRatiosAsync());
+            const source = picture.uri;
+
+            if(source) {
+                cameraRef.current.resumePreview();
+                console.log('picture source', source)
+            }   
+        }
+    }
 
     useEffect(() => {
         (async () => {
@@ -21,18 +39,29 @@ export default function CameraComponent() {
     }
 
     return (
-        <View style={styles.container}>
-            <Camera style={styles.camera} type={type}>
-                <View style={styles.buttonContainer}>
+        <View>
+            <Camera style={styles.camera} type={type} ref={cameraRef}>
+                <View style={{backgroundColor: 'transparent', flexDirection: 'row'}}>
+                    <View style={{flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'flex-end', paddingLeft: 5, paddingBottom: 5,}}>
+                        <View>
+                        <TouchableOpacity
+                        style={styles.button}
+                        onPress={takePicture}
+                        >
+                            <Icon name='aperture' size={50} color='white' />
+                    </TouchableOpacity>
+                        </View>
+                    </View>
                     <TouchableOpacity
                         style={styles.button}
-                        onPress={()=>{
-                            setType(
-                                type === Camera.Constants.type.back
-                                ? Camera.Constants.Type.front
-                                : Camera.Constants.Type.back
-                            );
-                        }}>
+                        // onPress={()=>{
+                        //     setType(
+                        //         type === Camera.Constants.type.back
+                        //         ? Camera.Constants.Type.front
+                        //         : Camera.Constants.Type.back
+                        //     );
+                        // }}>
+                        >
                         <Text style={styles.text}> Flip </Text>
                     </TouchableOpacity>
                 </View>
@@ -46,9 +75,9 @@ const styles = StyleSheet.create({
         height: '100%',
         width: '100%',
     },
-    // camera: {
-    //     flex: 1,
-    // },
+    camera: {
+        // flexDirection: 'row',
+    },
     // buttonContainer: {
     //     flex: 1,
     //     backgroundColor: 'transparent',
